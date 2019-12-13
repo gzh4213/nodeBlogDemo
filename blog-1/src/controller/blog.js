ac7c00611363506d9a2672dec0bc19d27ffa15e6
guozhen <guozhen4213@126.com>
2019-12-14 01:24:11 +0800

    博客新建/更新/删除路由

--- blog-1/src/controller/blog.js
+++ blog-1/src/controller/blog.js
@@ -1,3 +1,8 @@
+/**
+ * 获取博客列表
+ * @param {作者} author 
+ * @param {关键字} keyword 
+ */
 const getList = (author, keyword) => {
     // 先返回假数据（格式是正确的）
 
@@ -19,6 +24,10 @@ const getList = (author, keyword) => {
     ]
 }
 
+/**
+ * 获取博客详情
+ * @param {博客ID} id 
+ */
 const getDetail = (id) => {
     // 先返回假数据
     return {
@@ -30,7 +39,42 @@ const getDetail = (id) => {
     }
 }
 
+/**
+ * 新建一篇博客
+ * @param {新建博客的数据} blogData 
+ */
+const newBlog = (blogData = {}) => {
+    // blogData 是一个博客对象，包含 title content 属性
+    return {
+        id: 3  // 表示新建博客，插入到数据库表里面的ID
+    }
+}
+
+/**
+ * 更新一篇博客
+ * @param {博客ID} id 
+ * @param {博客数据}} blogData 
+ */
+const updateBlog = (id, blogData = {}) => {
+    // id 就是要更新博客的 id
+    // blogData 是一个博客对象，包含 title content 属性
+    return true
+}
+
+/**
+ * 删除一篇博客
+ * @param {博客ID} id 
+ */
+const delBlog = (id) => {
+    // id 就是要更新博客的 id
+
+    return true
+}
+
 module.exports = {
     getList,
-    getDetail
+    getDetail,
+    newBlog,
+    updateBlog,
+    delBlog
 }
