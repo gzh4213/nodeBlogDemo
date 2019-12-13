fb8c9c6def4d778a2df3ca17591f6fe0ff76a25c
guozhen <guozhen4213@126.com>
2019-12-14 00:52:33 +0800

    博客详情路由

--- blog-1/src/controller/blog.js
+++ blog-1/src/controller/blog.js
@@ -19,6 +19,18 @@ const getList = (author, keyword) => {
     ]
 }
 
+const getDetail = (id) => {
+    // 先返回假数据
+    return {
+        id: 1,
+        title: '标题A',
+        content: '内容A',
+        createTime: 1575910161772,
+        author: 'zhangsan'
+    }
+}
+
 module.exports = {
-    getList
+    getList,
+    getDetail
 }
