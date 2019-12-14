7ba8dc2d85eba5495b1dfef4641a6b42b77e4063
guozhen <guozhen4213@126.com>
2019-12-15 01:15:48 +0800

    配置mysql

---
+++ blog-1/src/conf/db.js
@@ -0,0 +1,29 @@
+const env = process.env.NODE_ENV   // 环境参数
+
+// 配置
+let MYSQL_CONF
+
+if (env === 'dev') {
+    MYSQL_CONF = {
+        host: 'localhost',
+        user: 'root',
+        password: 'Guo4213!',
+        port:'3306',
+        database: 'myblog'
+    }
+}
+
+if (env === 'production') {
+    MYSQL_CONF = {
+        host: 'localhost',
+        user: 'root',
+        password: 'Guo4213!',
+        port:'3306',
+        database: 'myblog'
+    }
+}
+
+
+module.exports = {
+    MYSQL_CONF
+}
