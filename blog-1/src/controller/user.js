8540ffe682c0eab56c83c11cf89166c13b5aa4fa
guozhen <guozhen4213@126.com>
2019-12-17 01:07:20 +0800

    [feat] 使用cookie登录验证,接入session中

--- blog-1/src/controller/user.js
+++ blog-1/src/controller/user.js
@@ -4,7 +4,7 @@ const { exec } = require('../db/mysql')
  * @param {用户名} username 
  * @param {密码} password 
  */
-const loginCheck = (username, password) => {
+const login = (username, password) => {
     const sql = `
         select username, realname from users where username='${username}' and password='${password}';
     `
@@ -15,5 +15,5 @@ const loginCheck = (username, password) => {
 }
 
 module.exports = {
-    loginCheck
+    login
 }
