2df7414d45181324e536addaa037d3fd47fc138e
guozhen <guozhen4213@126.com>
2020-01-08 00:26:33 +0800

    express get&post test

--- blog-express/app.js
+++ blog-express/app.js
@@ -6,6 +6,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const blogRouter = require('./routes/blog')
+const userRouter = require('./routes/user')
 
 var app = express();
 
@@ -26,6 +28,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 // 注册路由
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/api/blog', blogRouter)
+app.use('/api/user', userRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
