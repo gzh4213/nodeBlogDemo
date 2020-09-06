ef76322c75c7e94c1a9b43034d05dcb8e9e084e9
guozhen <guozhen4213@126.com>
2020-09-06 16:27:48 +0800

    开发路由-代码演示

--- blog-koa/routes/user.js
+++ blog-koa/routes/user.js
@@ -1,29 +1,34 @@
 const router = require('koa-router')()
+const { login } = require('../controller/user')
+const { SuccessModel, ErrorModel } = require('../model/resModel')
 
 router.prefix('/api/user')
 
 router.post('/login', async function (ctx, next) {
-  console.log(ctx.request)
   const { username, password } = ctx.request.body
-  ctx.body = {
-    errno: 0,
-    username,
-    password
+  const data = await login(username, password)
+  if (data.username) {
+    // 设置 session
+    ctx.session.username = data.username
+    ctx.session.realname = data.realname
+    ctx.body = new SuccessModel()
+    return
   }
+  ctx.body = new ErrorModel('登录失败')
 })
 
-router.get('/session-test', async function (ctx, next) {
-  if(ctx.session.viewCount == null) {
-    ctx.session.viewCount = 0
-  }
+// router.get('/session-test', async function (ctx, next) {
+//   if(ctx.session.viewCount == null) {
+//     ctx.session.viewCount = 0
+//   }
 
-  ctx.session.viewCount++
+//   ctx.session.viewCount++
 
-  ctx.body = {
-    errno: 0,
-    viewCount: ctx.session.viewCount
-  }
-})
+//   ctx.body = {
+//     errno: 0,
+//     viewCount: ctx.session.viewCount
+//   }
+// })
 
 
 module.exports = router
